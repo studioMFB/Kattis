0c7d73445077be6df4965061811e31ab49fd63d5
studiomfb <57592762+studioMFB@users.noreply.github.com>
2020-02-05 18:02:40 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kattis_Rotate_And_Cut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < cases; i++)
+            {
+                string[] line = Console.ReadLine().Split(' ');
+                int rotation = int.Parse(line[0]);
+                string message = line[1];
+
+                for(int j = 0; j < rotation; j++)
+                {
+                    if (message.Length < 4)
+                    {
+                        break;
+                    }
+                    message = message.Remove(0, (message.Length / 4));
+                }
+
+                Console.WriteLine(message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
